d5bd494de682736905213447ab40123ac51bb1be
bunsai <bunsai090saporno@gmail.com>
2025-06-23 15:24:05 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-// import LoginPage from './components/LoginPage/LoginPage'
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import LoginPage from './login';
+import AddClassroom from './components/AddClassroom';
+import AddStudent from './components/AddStudent';
+import AddTeacher from './components/AddTeacher';
+import AddAdmin from './components/AddAdmin';
 
 function App() {
   return (
-    <div className="app">
-      {/* <LoginPage /> */}
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/add-student" element={<AddStudent />} />
+      <Route path="/add-classroom" element={<AddClassroom />} />
+      <Route path="/add-teacher" element={<AddTeacher />} />
+      <Route path="/add-admin" element={<AddAdmin />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App; 
